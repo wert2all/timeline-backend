0d151633b5cbc2681d6424ff55fa4a739b353e54
wert2all <wert2all@gmail.com>
2024-04-17 17:59:26 +0300

    feat: etract token from request

---
+++ auth/auth.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+)
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			token := extractToken(req)
+			if token == "" {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			// // Allow unauthenticated users in
+			// if err != nil || c == nil {
+			// 	next.ServeHTTP(w, r)
+			// 	return
+			// }
+
+			// userId, err := validateAndGetUserID(c)
+			// if err != nil {
+			// 	http.Error(w, "Invalid cookie", http.StatusForbidden)
+			// 	return
+			// }
+
+			// // get the user from the database
+			// user := getUserByID(db, userId)
+
+			// // put it in context
+			// ctx := context.WithValue(r.Context(), userCtxKey, user)
+
+			// // and call the next with our new context
+			// r = r.WithContext(ctx)
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func extractToken(req *http.Request) string {
+	authHeader := req.Header.Get("Authorization")
+	splitted := strings.Split(authHeader, " ")
+
+	if len(splitted) == 2 {
+		return splitted[1]
+	} else {
+		return ""
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.9
 
 require (
 	github.com/99designs/gqlgen v0.17.45
+	github.com/go-chi/chi v1.5.5
 	github.com/vektah/gqlparser/v2 v2.5.11
 )
 
