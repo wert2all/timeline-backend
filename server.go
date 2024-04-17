48ae7ee50fa8030c033cbfdadcff9bb8f046b70d
wert2all <wert2all@gmail.com>
2024-04-17 17:59:26 +0300

    feat: etract token from request

--- server.go
+++ server.go
@@ -3,15 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"timeline/backend/auth"
 	"timeline/backend/graph"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/go-chi/chi"
 )
 
-const defaultPort = "3000"
+const defaultPort = "8000"
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(auth.Middleware())
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/graphql", srv)
 	http.Handle("/graphql", srv)
-	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
+	log.Fatal(http.ListenAndServe(":"+defaultPort, router))
 }
