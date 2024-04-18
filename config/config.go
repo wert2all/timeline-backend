7ead494cfca8cae033487fb1912974bd237571e3
wert2all <wert2all@gmail.com>
2024-04-18 16:13:15 +0300

    refactor: refactor middlewares

--- config/config.go
+++ config/config.go
@@ -2,10 +2,18 @@ package config
 
 import "github.com/sakirsensoy/genv"
 
+type CORS struct {
+	AllowedOrigin string
+	Debug         bool
+}
 type appConfig struct {
 	Port string
+	CORS CORS
 }
 
 var AppConfig = &appConfig{
 	Port: genv.Key("PORT").Default("8000").String(),
+	CORS: CORS{
+		Debug: genv.Key("CORS_DEBUG").Default(false).Bool(), AllowedOrigin: genv.Key("CORS_ALLOWED_ORIGIN").String(),
+	},
 }
