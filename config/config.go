7e690507bf39df41445dc0bd2de7e53b6c5db7c3
Serhii Shvets <wert2all@gmail.com>
2024-04-18 21:40:20 +0300

    feat: add postgres settings to config

--- config/config.go
+++ config/config.go
@@ -9,9 +9,19 @@ type CORS struct {
 	AllowedOrigin string
 	Debug         bool
 }
+
+type Postgres struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+
 type appConfig struct {
 	Port          string
 	CORS          CORS
+	Postgres      Postgres
 	GoogleClintID string
 }
 
@@ -20,6 +30,13 @@ var AppConfig = &appConfig{
 	CORS: CORS{
 		Debug:         genv.Key("CORS_DEBUG").Default(false).Bool(),
 		AllowedOrigin: genv.Key("CORS_ALLOWED_ORIGIN").String(),
+	},
+	Postgres: Postgres{
+		Host:     genv.Key("POSTGRES_HOST").Default("localhost").String(),
+		Port:     genv.Key("POSTGRES_PORT").Default(5432).Int(),
+		User:     genv.Key("POSTGRES_USER").String(),
+		Password: genv.Key("POSTGRES_PASSWORD").String(),
+		Database: genv.Key("POSTGRES_DB").String(),
 	},
 	GoogleClintID: genv.Key("GOOGLE_CLIENT_ID").String(),
 }
