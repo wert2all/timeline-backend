353dd9596d07a7dc13619aaf65ba9a3629406ec6
wert2all <wert2all@gmail.com>
2024-04-18 17:27:10 +0300

    fix: fix reading dotenv variables

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "github.com/sakirsensoy/genv"
+import (
+	"github.com/sakirsensoy/genv"
+	_ "github.com/sakirsensoy/genv/dotenv/autoload"
+)
 
 type CORS struct {
 	AllowedOrigin string
@@ -15,8 +18,8 @@ type appConfig struct {
 var AppConfig = &appConfig{
 	Port: genv.Key("PORT").Default("8000").String(),
 	CORS: CORS{
-		Debug: genv.Key("CORS_DEBUG").Default(false).Bool(), AllowedOrigin: genv.Key("CORS_ALLOWED_ORIGIN").String(),
+		Debug:         genv.Key("CORS_DEBUG").Default(false).Bool(),
+		AllowedOrigin: genv.Key("CORS_ALLOWED_ORIGIN").String(),
 	},
-
 	GoogleClintID: genv.Key("GOOGLE_CLIENT_ID").String(),
 }
