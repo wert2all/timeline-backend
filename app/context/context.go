dbdcfe2024719da80028e0f3aa90174e55d6b912
Serhii Shvets <wert2all@gmail.com>
2024-04-20 11:33:39 +0300

    refactor: add models to context

--- app/context/context.go
+++ app/context/context.go
@@ -1,3 +1,48 @@
 package context
 
+import (
+	"context"
+	"timeline/backend/db/model/user"
+)
+
 type AppContextUserKey struct{}
+
+type (
+	AppModelsKey struct{}
+	AppModels    struct {
+		Users user.UserModel
+	}
+)
+
+type AppContext interface {
+	GetContext() context.Context
+	GetModels() AppModels
+	SetUserID(id int) context.Context
+	GetUserID() *int
+}
+
+type AppContextImpl struct {
+	context context.Context
+}
+
+// AddUserId implements AppContext.
+func (a AppContextImpl) SetUserID(id int) context.Context {
+	a.context = context.WithValue(a.context, AppContextUserKey{}, id)
+	return a.GetContext()
+}
+
+func (a AppContextImpl) GetUserID() *int {
+	value := a.context.Value(AppContextUserKey{})
+	if value == nil {
+		return nil
+	}
+	return value.(*int)
+}
+func (a AppContextImpl) GetContext() context.Context { return a.context }
+func (a AppContextImpl) GetModels() AppModels        { return a.context.Value(AppModelsKey{}).(AppModels) }
+
+func NewAppContext(ctx context.Context, models AppModels) AppContext {
+	return AppContextImpl{context: context.WithValue(ctx, AppModelsKey{}, models)}
+}
+
+func NewModels(userModel user.UserModel) AppModels { return AppModels{Users: userModel} }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	entgo.io/ent v0.13.1
 	github.com/99designs/gqlgen v0.17.45
 	github.com/go-chi/chi v1.5.5
+	github.com/lib/pq v1.10.9
 	github.com/rs/cors v1.10.1
 	github.com/sakirsensoy/genv v1.0.1
 	github.com/vektah/gqlparser/v2 v2.5.11
