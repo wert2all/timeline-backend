5385b81e08434e6b1cd26a2d6e5e91eaeecbad77
Serhii Shvets <wert2all@gmail.com>
2024-05-16 11:05:35 +0300

    feat: add tag repository to sl and to model

--- db/model/event/event.go
+++ db/model/event/event.go
@@ -3,6 +3,7 @@ package event
 import (
 	"time"
 	"timeline/backend/db/repository/event"
+	"timeline/backend/db/repository/tag"
 	"timeline/backend/ent"
 	entEvent "timeline/backend/ent/event"
 )
@@ -14,18 +15,21 @@ type Model interface {
 }
 
 type modelImpl struct {
-	repository event.Repository
+	eventRepository event.Repository
+	tagRepository   tag.Repository
 }
 
-func (m modelImpl) GetEventByID(id int) (*ent.Event, error) { return m.repository.FindByID(id) }
+func (m modelImpl) GetEventByID(id int) (*ent.Event, error) { return m.eventRepository.FindByID(id) }
 
 // Update implements Model.
 func (m modelImpl) Update(event *ent.EventUpdateOne) (*ent.Event, error) {
-	return m.repository.UpdateEvent(event)
+	return m.eventRepository.UpdateEvent(event)
 }
 
 func (m modelImpl) Create(date time.Time, eventType entEvent.Type) (*ent.Event, error) {
-	return m.repository.CreateEvent(date, eventType)
+	return m.eventRepository.CreateEvent(date, eventType)
 }
 
-func NewEventModel(repository event.Repository) Model { return modelImpl{repository: repository} }
+func NewEventModel(eventRepository event.Repository, tagRepository tag.Repository) Model {
+	return modelImpl{eventRepository: eventRepository, tagRepository: tagRepository}
+}
