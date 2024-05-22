913a8a0139cee579bd1e9dba575ce0ae1033d295
Serhii Shvets <wert2all@gmail.com>
2024-05-22 23:01:54 +0300

    fix: remove useless function

--- di/di.go
+++ di/di.go
@@ -1,12 +1,5 @@
 package di
 
-import (
-	"timeline/backend/db/model/event"
-	"timeline/backend/db/model/tag"
-	"timeline/backend/db/model/timeline"
-	"timeline/backend/db/model/user"
-)
-
 type Postgres struct {
 	Port     string `yaml:"port"`
 	Host     string `yaml:"host"`
@@ -32,16 +25,4 @@ type Config struct {
 	Sentry struct {
 		Dsn string `yaml:"dsn"`
 	} `yaml:"sentry"`
-}
-
-// Models todo refactor
-type Models struct {
-	Users    user.UserModel
-	Timeline timeline.UserTimeline
-	Event    event.Model
-	Tag      tag.Model
-}
-
-func NewAllModels(user user.UserModel, timeline timeline.UserTimeline, event event.Model, tag tag.Model) Models {
-	return Models{Users: user, Timeline: timeline, Event: event, Tag: tag}
 }
