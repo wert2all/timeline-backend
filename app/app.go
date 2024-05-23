cbf71f3a6938e93fa05b47fd969bed38134a05e9
Serhii Shvets <wert2all@gmail.com>
2024-05-23 12:41:31 +0300

    refactor: refactor  creating application

--- app/app.go
+++ app/app.go
@@ -3,8 +3,6 @@ package app
 import (
 	"log"
 	"net/http"
-	"timeline/backend/app/router"
-	"timeline/backend/di"
 
 	"github.com/go-chi/chi"
 )
@@ -21,6 +19,6 @@ func (a *app) Start() {
 	log.Fatal(http.ListenAndServe(":8000", a.router))
 }
 
-func NewApplication(locator di.ServiceLocator) Application {
-	return &app{router: router.NewRouterFactory(locator).Create()}
+func NewApplication(router chi.Router) Application {
+	return &app{router: router}
 }
