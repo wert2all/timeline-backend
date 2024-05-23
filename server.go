310e9fdd063e43f4c49103ba5fe2bcef113bd837
Serhii Shvets <wert2all@gmail.com>
2024-05-23 12:41:31 +0300

    refactor: refactor  creating application

--- server.go
+++ server.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"timeline/backend/app"
+	"timeline/backend/app/router"
 	"timeline/backend/di"
 )
 
 func main() {
 	serviceLocator := di.Init()
 
-	application := app.NewApplication(serviceLocator)
+	application := app.NewApplication(router.NewRouterFactory(serviceLocator).Create())
 	application.Start()
 
 	defer serviceLocator.Close()
