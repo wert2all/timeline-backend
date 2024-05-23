645a7722b0d3d2fd1b36b24e2e3c9500048ce55b
Serhii Shvets <wert2all@gmail.com>
2024-05-23 12:25:39 +0300

    fix: move creating client etc to di

--- server.go
+++ server.go
@@ -1,45 +1,15 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-	"time"
 	"timeline/backend/app"
-	"timeline/backend/db"
 	"timeline/backend/di"
-
-	"github.com/getsentry/sentry-go"
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	appConfig := readConfig()
-	client := db.NewClient(appConfig.Postgres)
-	defer client.Close()
+	serviceLocator := di.Init()
 
-	err := sentry.Init(sentry.ClientOptions{Dsn: appConfig.Sentry.Dsn, Debug: true})
-	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
-	}
-	defer sentry.Flush(time.Second)
+	application := app.NewApplication(serviceLocator)
+	application.Start()
 
-	app.NewApplication(di.NewServiceLocator(appConfig, context.Background(), client)).Start()
-}
-
-func readConfig() di.Config {
-	f, err := os.Open("config.yaml")
-	if err != nil {
-		panic("cannot open config file")
-	}
-	defer f.Close()
-
-	var cfg di.Config
-	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(&cfg)
-	if err != nil {
-		panic("cannot parse config ")
-	}
-
-	return cfg
+	defer application.Close()
 }
