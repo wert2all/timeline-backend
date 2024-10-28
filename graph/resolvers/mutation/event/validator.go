6e33a20bc15724af03612f0da3aaa216a8c4c0d2
wert2all <wert2all@gmail.com>
2024-10-28 10:19:57 +0200

    refactor: extract validation of base event input type

---
+++ graph/resolvers/mutation/event/validator.go
@@ -0,0 +1,102 @@
+package event
+
+import (
+	"context"
+	"net/url"
+	"time"
+
+	appContext "timeline/backend/app/context"
+	"timeline/backend/db/model/timeline"
+	"timeline/backend/ent"
+	entEvent "timeline/backend/ent/event"
+	"timeline/backend/graph/model"
+	"timeline/backend/lib/utils"
+
+	"golang.org/x/exp/maps"
+	"github.com/microcosm-cc/bluemonday"
+)
+
+type (
+	BaseValidator interface {
+		GetBaseValidEventInput(GQLInput, context.Context) (*BaseValidEventInput, error)
+	}
+	GQLInput struct {
+		ID          *int
+		TimelineID  int
+		Date        time.Time
+		Type        *model.TimelineType
+		Title       *string
+		Description *string
+		ShowTime    *bool
+		URL         *string
+		Tags        []string
+	}
+
+	BaseValidEventInput struct {
+		Timeline    *ent.Timeline
+		EventType   entEvent.Type
+		Date        time.Time
+		Title       string
+		Description string
+		ShowTime    bool
+		Url         *url.URL
+		Tags        []string
+	}
+
+	baseValidatorImpl struct {
+		timelineModel timeline.UserTimeline
+	}
+)
+
+func NewBaseValidator(timelineModel timeline.UserTimeline) BaseValidator {
+	return baseValidatorImpl{timelineModel: timelineModel}
+}
+
+func (b baseValidatorImpl) GetBaseValidEventInput(input GQLInput, ctx context.Context) (*BaseValidEventInput, error) {
+	p := bluemonday.StrictPolicy()
+	timelineEntity, err := b.timelineModel.GetUserTimeline(appContext.GetUserID(ctx), input.TimelineID)
+	if err != nil {
+		return nil, err
+	}
+	var eventType entEvent.Type
+	if input.Type == nil {
+		eventType = entEvent.Type(model.TimelineTypeDefault)
+	} else {
+		eventType = entEvent.Type(input.Type.String())
+	}
+
+	groupedTags := make(map[string]string)
+	for _, tagInput := range input.Tags {
+		groupedTags[p.Sanitize(tagInput)] = p.Sanitize(tagInput)
+	}
+	var showTime bool
+	if input.ShowTime != nil {
+		showTime = *input.ShowTime
+	} else {
+		showTime = true
+	}
+
+	return &BaseValidEventInput{
+		Timeline:    timelineEntity,
+		EventType:   eventType,
+		Date:        input.Date,
+		Title:       p.Sanitize(utils.DerefString(input.Title)),
+		Description: p.Sanitize(utils.DerefString(input.Description)),
+		ShowTime:    showTime,
+		Url:         b.getLink(input.URL),
+		Tags:        maps.Values(groupedTags),
+	}, nil
+}
+
+func (b baseValidatorImpl) getLink(link *string) *url.URL {
+	linkString := utils.DerefString(link)
+	if linkString != "" {
+		link, err := url.ParseRequestURI(linkString)
+		if err != nil {
+			panic(err)
+		}
+		return link
+	} else {
+		return nil
+	}
+}
